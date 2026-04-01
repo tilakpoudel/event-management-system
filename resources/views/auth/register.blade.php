2d3eb8733981c0cbf7e86c65d7e7b2d607ed45a3
tilak <tilak.poudel.314@gmail.com>
2026-04-01 14:02:24 +0545

    feat: register user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,18 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        {{-- Role --}}
+        <div class="mt-4">
+            <x-input-label for="role" :value="__('Role')" />
+
+            <select id="role" name="role" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <option value="organizer" {{ old('role') == 'organizer' ? 'selected' : '' }}>Organizer</option>
+                <option value="attendee" {{ old('role') == 'attendee' ? 'selected' : '' }}>Attendee</option>
+            </select>
+
+            <x-input-error :messages="$errors->get('role')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
